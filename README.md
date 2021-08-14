e756684742ea9ad532477f7f40db69266c6cd26b
k-cermak <karel4cermak@seznam.cz>
2021-08-14 03:27:43 +0200

    Changing repository structure

---
+++ README.md
@@ -0,0 +1,25 @@
+# Enplated
+## Simplify web development and its boring basics with Enplated framework
+
+### Why to use Enplated?
+- HTML variables
+- Simple font and color settings
+- Integrated element blocks
+- Integrated layout including mobile layout
+- No server side integration
+- Completely free
+
+### Enplated website:
+https://enplated.k-cermak.com
+
+### How to implement Enplated?
+https://enplated.k-cermak.com/en/implementation
+
+### Where can I read documentation?
+https://enplated.k-cermak.com/en/documentation
+
+### License
+Enplated is available under MIT license. More here: https://enplated.k-cermak.com/en/license
+
+### Sources used:
+SpinKit by tobiasahlin: https://github.com/tobiasahlin/SpinKit
---
+++ config.js
@@ -0,0 +1,53 @@
+//THIS IS CONFIG FILE OF ENPLATED SITE
+//YOU SHOULDN'T DO ANY CHANGES IN THIS FILE ON LIVE PAGE
+
+//DECLARE YOUR PAGE VARIABLES
+var pageVariables = {
+    pageName : 'My Page',
+    pageDesc : 'My Amazing Page created with Enplated'
+}
+
+//DECLARE YOUR ELEMENTS COLORS. USE default FOR DEFAULT COLORS
+var pageColors = {
+    mainBackgroud : '#222222',
+    secondBackground : '#393e46',
+    icon: '#ffffff',
+    h1 : '#ffffff',
+    h2 : '#ffffff',
+    h3 : '#ffffff',
+    h4 : '#ffffff',
+    h5 : '#ffffff',
+    h6 : '#ffffff',
+    p : '#ffffff',
+    a : '#03a9f4',
+    aHover : '#0072a6',
+    headerName : '#ffffff',
+    headerMenu : '#ffffff',
+    headerMenuMobileBackground : '#121212',
+    footerMain : '#393e46',
+    footerSecond : '#121212',
+}
+
+//DECLARE YOUR ELEMENTS FONTS. USE default FOR DEFAULT FONTS
+var fonts = {
+    h1 : 'Arial',
+    h2 : 'Arial',
+    h3 : 'Arial',
+    h4 : 'Arial',
+    h5 : 'Arial',
+    h6 : 'Arial',
+    p : 'Arial',
+    headerName : 'Arial',
+    headerMenu : 'Arial',
+    footerMain : 'Arial',
+    footerSecond : 'Arial',
+}
+
+//ELEMENT ID FOR COPYRIGHT TEXT
+var copyrightInfoId = 'id';
+
+//IF ENABLED, YOU CAN OPEN CONSOLE AND WATCH DEVELOPER INFORMATIONS
+var developerVersion = false;
+
+//IMPORT YOUR GOOGLE FONTS (https://fonts.google.com). GOOGLE MATERIAL ICONS IS ALREADY IMPORTED
+var googleFontsImport = 'https://fonts.googleapis.com/css2?family=Alata&family=Roboto:wght@100&display=swap';
