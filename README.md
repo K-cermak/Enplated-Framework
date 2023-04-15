6cb014da8985ec6e21a41faca02ba1a5333b52ca
EnplatedSyncer <karel4cermak@seznam.cz>
2023-04-16 00:48:23 +0200

    Added flash, aos and loading icons support

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 - [✅] Add Bootstrap 5.3 loader (including the icons)
 - Add Bootstrap modal functions
 - [✅] Add Flash messages functions
-- Add AOS library
-- Add Loading animations functions
+- [✅] Add AOS library
+- [✅] Add Loading animations functions
 - Add GDPR and cookie consent functions
 - Smart variables
+- Loading screen?
--- enp-loader.js
+++ enp-loader.js
@@ -1,7 +1,19 @@
 var enplatedSettingsCustom = {
     useDarkMode : false,
     importFlash : true,
-    
+    importAOS : true,
+    dataSetter : {
+        basicAnimation : {
+            selector : ".basic-animation",
+            "data-aos" : "zoom-in",
+            "data-aos-delay" : "300",
+        },
+        anotherAnimation : {
+            selector : ".another-animation",
+            "data-aos" : "fade-up",
+            "data-aos-delay" : "300",
+        },
+    }
 };
 
 var enplatedSettingsImport = {
@@ -9,23 +21,94 @@ var enplatedSettingsImport = {
     bootstrapDarkCss : "https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css",
     bootstrapImportJs : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",
     bootstrapIcons : "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css",
-    flashJs : "/enp-loader/flash.min.js",
-    flashCss : "/enp-loader/flash.min.css",
+
+    flashJs : "/enp-data/flash.min.js",
+    flashCss : "/enp-data/flash.min.css",
+
+    aosJs : "https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.js",
+    aosCss : "https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.css",
+
+    loaderDataset : "/enp-data/loaders.js",
+    allowedLoaders : ["sk-plane", "sk-chase", "sk-bounce", "sk-wave", "sk-pulse", "sk-flow", "sk-swing", "sk-circle", "sk-circlefade", "sk-grid", "sk-fold", "sk-wander", "vl-simplewheel", "vl-onewheel", "vl-doublewheel", "vl-dotwheel", "vl-mutliwheel", "vl-lightwheel", "vl-line"]
 };
 
 start();
-function start() {
-    /*BOOTSTRAP LOAD*/
-    loadCss(enplatedSettingsImport.bootstrapImportCss);
-    if (enplatedSettingsCustom.useDarkMode) loadCss(enplatedSettingsImport.bootstrapDarkCss);
-    includeJs(enplatedSettingsImport.bootstrapImportJs);
-    loadCss(enplatedSettingsImport.bootstrapIcons);
 
-    /*FLASH LOAD*/
-    if (enplatedSettingsCustom.importFlash)includeJs(enplatedSettingsImport.flashJs);
-    if (enplatedSettingsCustom.importFlash) loadCss(enplatedSettingsImport.flashCss);
+function start() {
+    bootstrapLoad();
+    dataSetter();
+    if (enplatedSettingsCustom.importFlash) {
+        flashLoad();
+    }
+    if (enplatedSettingsCustom.importAOS) {
+        aosLoad();
+    }
+    loaderLoad();
+
 }
 
+/**********************************/
+/*START FUNCTIONS*/
+/**********************************/
+function bootstrapLoad() {
+    loadCss(enplatedSettingsImport.bootstrapImportCss);
+    if (enplatedSettingsCustom.useDarkMode) {
+        loadCss(enplatedSettingsImport.bootstrapDarkCss);
+    }
+    includeJs(enplatedSettingsImport.bootstrapImportJs);
+    loadCss(enplatedSettingsImport.bootstrapIcons);
+}
+
+function dataSetter() {
+    let allSetters = enplatedSettingsCustom.dataSetter;
+    for (let key in allSetters) {
+        let setter = allSetters[key];
+        let elements = document.querySelectorAll(setter.selector);
+
+        for (let j = 0; j < elements.length; j++) {
+            let element = elements[j];
+            for (let key in setter) {
+                if (key != 'selector') {
+                    element.setAttribute(key, setter[key]);
+                }
+            }
+        }
+    }
+}
+
+function flashLoad() {
+    includeJs(enplatedSettingsImport.flashJs);
+    loadCss(enplatedSettingsImport.flashCss);
+}
+
+function aosLoad() {
+    includeJs(enplatedSettingsImport.aosJs);
+    loadCss(enplatedSettingsImport.aosCss);
+    //after load
+    window.onload = function() {
+        AOS.init({
+            once: true,
+        });
+    };
+}
+
+function loaderLoad() {
+    for (let i = 0; i < enplatedSettingsImport.allowedLoaders.length; i++) {
+        let loader = enplatedSettingsImport.allowedLoaders[i];
+        if (document.querySelectorAll(loader).length > 0) {
+            includeJs(enplatedSettingsImport.loaderDataset).then(() => {
+                startAnimRender();
+            });
+            break;
+        }
+    }
+}
+
+
+
+/**********************************/
+/*HELPERS*/
+/**********************************/
 /*INCLUDE HELPERS*/
 async function includeJs(jsFilePath) {
     await new Promise((resolve) => {
@@ -62,4 +145,70 @@ function genFlashMessage(msg, type, time) {
         window.FlashMessage.warning(msg, { settings });
     else if (type == 'info')
         window.FlashMessage.info(msg, { settings });
+}
+
+/*LOADERS HELPER*/
+function genRandomId() {
+    let text = "";
+    let possible = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < 10; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+}
+
+function startAnimRender() {
+    for (let i = 0; i < enplatedSettingsImport.allowedLoaders.length; i++) {
+        let loader = enplatedSettingsImport.allowedLoaders[i];
+
+        for (let j = 0; j < document.querySelectorAll(loader).length; j++) {
+            //split loader name by dash
+            let loaderType = loader.split('-')[0];
+            let loaderName = loader.split('-')[1];
+
+            let loaderElement = document.querySelectorAll(loader)[j];
+            let loaderColor = loaderElement.getAttribute('color');
+            let randomId = genRandomId();
+
+            let loaderHtml, loaderCss;
+
+            if (loaderType == 'sk') {
+                loaderHtml = loadersData.spinkit[loaderName].html;
+                loaderCss = loadersData.spinkit[loaderName].css;
+                
+                //rename selector with random id
+                loaderHtml = loaderHtml.replaceAll("selector", randomId);
+                loaderCss = loaderCss.replaceAll("selector", randomId);
+
+                //if color is set, replace it
+                if (loaderColor) {
+                    loaderCss = loaderCss.replaceAll("#333", loaderColor);
+                }
+
+            } else if (loaderType == "vl") {
+                loaderHtml = "<span class='selector'></span>";
+                loaderCss = loadersData.vinloaders[loaderName].css;
+                let secondColor = loaderElement.getAttribute('color2');
+                let thirdColor = loaderElement.getAttribute('color3');
+
+                //if color is set, replace it
+                if (loaderColor) {
+                    loaderCss = loaderCss.replaceAll("#ff3d00", loaderColor);
+                }
+                if (secondColor) {
+                    loaderCss = loaderCss.replaceAll("#fff", secondColor);
+                }
+                if (thirdColor) {
+                    loaderCss = loaderCss.replaceAll("#eee", thirdColor);
+                }
+            }
+
+            //rename selector with random id
+            loaderHtml = loaderHtml.replaceAll("selector", randomId);
+            loaderCss = loaderCss.replaceAll("selector", randomId);
+
+            loaderElement.innerHTML = loaderHtml;
+            loaderElement.insertAdjacentHTML("beforeend", "<style>" + loaderCss + "</style>");
+        }
+    }
 }
