7187898ff0a18e8e103b8e1607359487e67655ee
EnplatedSyncer <karel4cermak@seznam.cz>
2023-07-19 02:49:50 +0200

    changed URLs for darkmode

--- enp-loader.js
+++ enp-loader.js
@@ -1,6 +1,6 @@
 var enplatedSettingsImport = {
     bootstrapImportCss : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-    bootstrapDarkCss : "https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css",
+    bootstrapDarkCss : "https://cdn.jsdelivr.net/gh/K-cermak/Enplated-Framework@enp-v3/enp-data/darkmode.min.css",
     bootstrapImportJs : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
     bootstrapIcons : "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css",
 
